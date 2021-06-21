2c405c39098bf8b92d6cd3b605570d95fbbd3d18
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-06-22 01:02:32 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner:  false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return SafeArea(
+      child: Scaffold(
+
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Data",
+                
+                ),
+              // ignore: deprecated_member_use
+              RaisedButton(onPressed: (){
+
+              })
+            ],
+          ),
+        ),
+      ));
+  }
+}
